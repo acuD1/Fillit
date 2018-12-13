88fa1d6adb9db56e7e99b6a904a146024678b685
acuD1 <arsciand@student.42.fr>
2018-12-13 08:44:30 +0100

    Little norme

--- includes/fillit.h
+++ includes/fillit.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   fillit.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: saneveu <saneveu@student.42.fr>            +#+  +:+       +#+        */
+/*   By: arsciand <arsciand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/28 11:21:43 by arsciand          #+#    #+#             */
-/*   Updated: 2018/12/12 03:53:15 by saneveu          ###   ########.fr       */
+/*   Updated: 2018/12/13 08:42:57 by arsciand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,26 +19,24 @@
 
 typedef struct  s_verif
 {
-    int     hash;
-    int     p;
-    int     eol;
-}               t_verif;
+	int		hash;
+	int		p;
+	int		eol;
+}				t_verif;
 
-typedef struct  s_tetri
+typedef struct	s_tetri
 {
-    char        coor[4][2];
-    char        letter;
-}               t_tetri;
+	char		coor[4][2];
+	char		letter;
+}				t_tetri;
 
-void	    fillit(void);
-list        ft_parser(char *file);
-int         ft_reader(int fd, char map[COL][ROW]);
-void	    ft_init(char map[COL][ROW], t_verif *verif);
-int		    next_tetri(int fd, char *buff);
-int         ft_verif(char *c, t_verif *verif);
-int		    nb_link(char map[COL][ROW]);
+void		fillit(void);
+list		ft_parser(char *file);
+int			ft_reader(int fd, char map[COL][ROW]);
+void		ft_init(char map[COL][ROW], t_verif *verif);
+int			next_tetri(int fd, char *buff);
+int			ft_verif(char *c, t_verif *verif);
+int			nb_link(char map[COL][ROW]);
 int			fill_list(char tab[COL][ROW], list first);
-
-
 
 #endif
